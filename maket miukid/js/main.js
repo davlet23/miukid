e19d690e73a0b3ba72b201ed8abd8e50e875ba57
dzheksembievdavlet <dzheksembievdavlet@gmail.com>
2021-10-26 23:15:34 +0600

    initial commit

---
+++ maket miukid/js/main.js
@@ -0,0 +1,44 @@
+ let a =+prompt ("Enter the number to add");
+ let b =+prompt ("Enter the number to add");
+ let result = a + b;  
+ alert(result);
+
+let c =+prompt ("Enter the number to subtract");
+let e =+prompt ("Enter the number to subtract");
+let resul = c - e;  
+alert(resul);
+
+let k =+prompt ("Enter the number to multiply");
+let m =+prompt ("Enter the number to multiply");
+let resu = k * m;
+alert(resu);
+
+let q =+prompt ("Enter the number to devide");
+let w =+prompt ("Enter the number to devide");
+let res = q / w;
+alert(res);
+
+let done =+prompt ("Enter the number to take the remainder of the devision");
+let don =+prompt ("Enter the number to take the reminder of the devision");
+let remainder = done % don;
+alert(remainder);
+
+let donAmar =+prompt ("Enter the number to rise in power");
+let donAmir =+prompt ("Enter the number to rise in power");
+let donon = donAmar ** donAmir;
+alert(donon);
+
+let login =prompt("Enter login","");
+if (login == "amazon"){
+alert("correct");
+} 
+else{
+    alert("incorrect");
+}
+let password =prompt("Enter password","");
+if (password == 123){
+    alert("correct");
+} 
+else{
+    alert("incorrect");
+}
