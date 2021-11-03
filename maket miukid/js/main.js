94dcb8858e560af763a75a33cd666b60645a5b6f
dzheksembievdavlet <dzheksembievdavlet@gmail.com>
2021-11-03 22:29:27 +0600

    lesson29

--- maket miukid/js/main.js
+++ maket miukid/js/main.js
@@ -1,56 +1,46 @@
- let a =+prompt ("Enter the number to add");
- let b =+prompt ("Enter the number to add");
- let result = a + b;  
- alert(result);
-
-let c =+prompt ("Enter the number to subtract");
-let e =+prompt ("Enter the number to subtract");
-let resul = c - e;  
-alert(resul);
-
-let k =+prompt ("Enter the number to multiply");
-let m =+prompt ("Enter the number to multiply");
-let resu = k * m;
-alert(resu);
-
-let q =+prompt ("Enter the number to devide");
-let w =+prompt ("Enter the number to devide");
-let res = q / w;
-alert(res);
-
-let done =+prompt ("Enter the number to take the remainder of the devision");
-let don =+prompt ("Enter the number to take the reminder of the devision");
-let remainder = done % don;
-alert(remainder);
-
-let donAmar =+prompt ("Enter the number to rise in power");
-let donAmir =+prompt ("Enter the number to rise in power");
-let donon = donAmar ** donAmir;
-alert(donon);
-
-let login =prompt("Enter login","");
-if (login == "amazon"){
-alert("correct");
-} 
-else{
-    alert("incorrect");
-}
-let password =prompt("Enter password","");
-if (password == 123){
-    alert("correct");
-} 
-else{
-    alert("incorrect");
-}
 let title = document.getElementsByClassName("title");
-console.log(title[0].innerText);
+for (let text of title){
+    console.log(text.innerText);
+}
+var elements = document.getElementsByClassName("inbag");
+var myFuction = function (){
+    var attribute = this.parentNode.parentNode.parentNode.querySelectorAll(".title");
+    alert(attribute[0].innerText);
+
+};
+for (var i = 0; i < elements.length; i++){
+    elements[i].addEventListener("click", myFuction, false);
+};
 
 
-let imgswt = document.getElementsByClassName("imgswt");
-console.log(imgswt);
 
-let price =  document.getElementsByClassName("price");
-console.log(price[0].innerText);
 
-let tooltip = document.getElementsByClassName("tooltip");
-console.log(tooltip[0].innerText);
+
+var elements = document.getElementsByClassName("scale");
+var myFuction = function (){
+    var attribute = this.parentNode.parentNode.parentNode.querySelectorAll(".price");
+    alert(attribute[0].innerText);
+
+};
+
+for (var i = 0; i < elements.length; i++){
+    elements[i].addEventListener("click", myFuction, false);
+};
+
+
+
+
+
+var elements = document.getElementsByClassName("heart");
+var myFuction = function (){
+    var attribute = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll("img");
+    alert(attribute[0]);
+
+};
+for (var i = 0; i < elements.length; i++){
+    elements[i].addEventListener("click", myFuction, false);
+};
+
+const textElement = document.querySelectorAll(".title")[1];
+const textElementC = textElement.innerHTML;
+textElement.innerHTML = `amazon`;
