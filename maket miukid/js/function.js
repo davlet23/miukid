98c3869b9325df80abaab276b7d9a91ed4b51b1c
dzheksembievdavlet <dzheksembievdavlet@gmail.com>
2021-11-08 20:23:45 +0600

    lesson32

--- maket miukid/js/function.js
+++ maket miukid/js/function.js
@@ -17,11 +17,21 @@
 // square();
 
 
-function rectangle(){
-    l =+prompt("Enter radius");
-    w =+prompt("Enter width");
-    s = l * w;
-    alert(s);
+// function rectangle(){
+//     l =+prompt("Enter radius");
+//     w =+prompt("Enter width");
+//     s = l * w;
+//     alert(s);
 
-}
-rectangle();
+// }
+// rectangle();
+
+
+
+// function round(){
+//     number =+prompt("Enter number...");
+//     number2 =+prompt("Enter number 2...");
+//     res = number + number2;
+//     alert(res.toPrecision(1));
+// }
+// round();
